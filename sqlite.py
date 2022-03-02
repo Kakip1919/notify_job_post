02fa39740592fb8721f384e49f206580fa02304d
Kakip1919 <invest9me@yahoo.co.jp>
2022-03-02 16:45:25 +0900

    add scrap_detail on dict

--- sqlite.py
+++ sqlite.py
@@ -1,16 +1,16 @@
 import sqlite3
 
-dbname = 'test.db'
+dbname = 'lancers.db'
 conn = sqlite3.connect(dbname, isolation_level=None)
 
 cursor = conn.cursor()
 
-sql = """CREATE TABLE IF NOT EXISTS test(id, name, date)"""
+sql = """CREATE TABLE IF NOT EXISTS lancers_data(id real, title, body text,price text, date)"""
 
 cursor.execute(sql)
 conn.commit()
 
-sql = """SELECT name FROM sqlite_master WHERE TYPE='table'"""
+sql = """SELECT title FROM lancers_data WHERE TYPE='table'"""
 
 for t in cursor.execute(sql):#for文で作成した全テーブルを確認していく
     print(t)
--- test.py
+++ test.py
@@ -1,14 +1,27 @@
 from time import sleep
 import requests
-import re
 from bs4 import BeautifulSoup
 
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
 headers = {'User-Agent': ua}
 
 
-def scrap_detail(post_urls):
-    for post_url in post_urls:
+def Get_Post_Urls():
+    post_urls = []
+    url = "https://www.lancers.jp/work/search/system?open=1&ref=header_menu&show_description=0&work_rank%5B%5D=0&work_rank%5B%5D=1&work_rank%5B%5D=2&work_rank%5B%5D=3"
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'lxml')
+    urls = soup.select("a", class_="c-media__title")
+    for url in urls:
+        if "/work/detail" in url.get("href"):
+            post_urls.append("https://www.lancers.jp/" + url.get("href"))
+    return post_urls
+
+
+def scrap_detail():
+    c = 1
+    scrap_data = {"title": {}, "body": {}, "price": {}}
+    for post_url in Get_Post_Urls():
         response = requests.get(post_url, headers=headers)
         soup = BeautifulSoup(response.text, 'lxml')
 
@@ -21,23 +34,14 @@ def scrap_detail(post_urls):
         price_elem = soup.select("meta")
         price_pos_left = price_elem[1].get("content").find("(")
         price_pos_right = price_elem[1].get("content").find(")")
-        price = price_elem[1].get("content")
+        price = price_elem[1].get("content")[1 + price_pos_left:price_pos_right]
 
-        scrap_data = {"title": title, "body": body, "price": price}
-
-
-def Get_Post_Urls():
-    post_urls = []
-    url = "https://www.lancers.jp/work/search/system?open=1&ref=header_menu&show_description=0&work_rank%5B%5D=0&work_rank%5B%5D=1&work_rank%5B%5D=2&work_rank%5B%5D=3"
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, 'lxml')
-    urls = soup.select("a", class_="c-media__title")
-    for url in urls:
-        if "/work/detail" in url.get("href"):
-            post_urls.append("https://www.lancers.jp/" + url.get("href"))
-
-    return post_urls
+        scrap_data["title"].setdefault("title_" + str(c), title)
+        scrap_data["body"].setdefault("body_" + str(c), body)
+        scrap_data["price"].setdefault("price_" + str(c), price)
+        c += 1
+    return scrap_data
 
 
 if __name__ == '__main__':
-    scrap_detail(Get_Post_Urls())
+    print(scrap_detail())
